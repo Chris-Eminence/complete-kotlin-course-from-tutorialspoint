d7c18d618f9f04ae03974d775dbd08e3e303dd90
Chris-Eminence <emichris380@gmail.com>
2020-09-14 15:18:05 -0700

    tab view implemented

--- CGPA-Calaculator/CGPA-Calaculator-master/app/src/main/java/com/africinnovate/cgpa_calculator/ui/home/HomeFragment.kt
+++ CGPA-Calaculator/CGPA-Calaculator-master/app/src/main/java/com/africinnovate/cgpa_calculator/ui/home/HomeFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.lifecycle.ViewModelProviders
+import androidx.viewpager.widget.ViewPager
 import com.africinnovate.cgpa_calculator.R
+import com.africinnovate.cgpa_calculator.ui.FirstSemester
+import com.africinnovate.cgpa_calculator.ui.SecondSemester
+import com.google.android.material.tabs.TabLayout
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+        private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,7 +29,56 @@ class HomeFragment : Fragment() {
 //        val textView: TextView = root.findViewById(R.id.text_home)
 //        homeViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
-//        })
+//        }
+        val tabLayout: TabLayout = root.findViewById(R.id.tabLayout)
+        val viewPager: ViewPager = root.findViewById(R.id.viewPager)
+
+        tabLayout.addTab(tabLayout.newTab().setText("First Semester"))
+        tabLayout.addTab(tabLayout.newTab().setText("Second Semester"))
+
+        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+
+        val adapter = MyAdapter (this, parentFragmentManager, tabLayout.tabCount)
+
+        viewPager.adapter = adapter
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewPager.currentItem = tab.position
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+
+
         return root
+    }
+}
+
+internal class MyAdapter(
+    var context: HomeFragment,
+    fm: FragmentManager,
+    var totalTabs: Int
+) :
+    FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                FirstSemester()
+            }
+            1 -> {
+                SecondSemester()
+            }
+
+            else -> getItem(position)
+        }
+    }
+    override fun getCount(): Int {
+        return totalTabs
     }
 }
